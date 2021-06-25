710eca08c1a6777cde5a8c06e0785f564493259e
Oleg Teslenko <orlantostello@gmail.com>
2021-06-25 23:20:42 +0300

    Бинарный поиск через рекурсию

--- 2_binary_search.js
+++ 2_binary_search.js
@@ -26,3 +26,19 @@ function binarySearch(array, item) {
 
 console.log(binarySearch(array, 5));
 console.log("count = ", count);
+
+function recursiveBinarySearch(array, item, start, end) {
+  let middle = Math.floor((start + end) / 2);
+  count += 1;
+  if (item === array[middle]) {
+    return middle;
+  }
+  if (item < array[middle]) {
+    return recursiveBinarySearch(array, item, 0, middle - 1);
+  } else {
+    return recursiveBinarySearch(array, item, middle + 1, end);
+  }
+}
+
+console.log(recursiveBinarySearch(array, 0, 0, array.length));
+console.log(count);
